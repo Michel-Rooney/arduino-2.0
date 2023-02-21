f67438296ac6130d0b179302e76413795a6109aa
Michel-Rooney <michelrooney16@gmail.com>
2023-02-21 05:12:51 -0300

    Update

--- teste-esp-banco/teste-esp-banco.ino
+++ teste-esp-banco/teste-esp-banco.ino
@@ -12,18 +12,19 @@
 //const char WIFI_SSID[] = "Marqueza";
 //const char WIFI_PASSWORD[] = "19381980";
 
-//const char WIFI_SSID[] = "NetOnda=ITAREMA";
-//const char WIFI_PASSWORD[] = "Rooney06";
+const char WIFI_SSID[] = "NetOnda=ITAREMA";
+const char WIFI_PASSWORD[] = "Rooney06";
 
 //const char WIFI_SSID[] = "JOEL10 9678";
 //const char WIFI_PASSWORD[] = "joel12344321";
 
-const char WIFI_SSID[] = "AP02_VALLEY";
-const char WIFI_PASSWORD[] = "amontada";
+//const char WIFI_SSID[] = "AP02_VALLEY";
+//const char WIFI_PASSWORD[] = "amontada";
 
-String HOST_NAME = "https://teste-arduino-banco.onrender.com"; // change to your PC's IP address
-String PATH_NAME   = "/teste";
-String queryString = "/04/";
+String HOST_NAME = "http://192.168.3.11:8000/"; // change to your PC's IP address
+String PATH_NAME = "arduino/heart_rate/?";
+String USER_CODE = "code=aa79b1970021a51c3eed9a6409d883cbcecb73896f10277a5a93900b183ae880";
+String queryString = "&beat=10";
 
 void setup() {
   Serial.begin(9600); 
@@ -40,8 +41,7 @@ void setup() {
   Serial.println(WiFi.localIP());
   
   HTTPClient http;
-
-  http.begin(HOST_NAME + PATH_NAME + queryString); //HTTP
+  http.begin(HOST_NAME + PATH_NAME + USER_CODE + queryString); //HTTP
   int httpCode = http.GET();
 
   // httpCode will be negative on error
@@ -62,5 +62,23 @@ void setup() {
 }
 
 void loop() {
+  HTTPClient http;
+  http.begin(HOST_NAME + PATH_NAME + USER_CODE + queryString); //HTTP
+  int httpCode = http.GET();
 
+  // httpCode will be negative on error
+  if(httpCode > 0) {
+    // file found at server
+    if(httpCode == HTTP_CODE_OK) {
+      String payload = http.getString();
+      Serial.println(payload);
+    } else {
+      // HTTP header has been send and Server response header has been handled
+      Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+    }
+  } else {
+    Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+  }
+
+  http.end();
 }
